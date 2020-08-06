1f873f0b34d70c1514d44fcd8f6c6b482eb8ae87
mrmapaia.3@gmail.com <mrmapaia.3@gmail.com>
2020-08-06 20:15:27 +0430

    models modified

--- market/core/models.py
+++ market/core/models.py
@@ -2,10 +2,14 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
-class Customer(User):
+class Person(User):
     phone_number = models.CharField(max_length=13, blank=False)
     address = models.CharField(max_length=50, blank=False)
     national_ID = models.CharField(max_length=10, blank=False)
+
+
+class Customer(Person):
+    pass
 
 
 class Product(models.Model):
@@ -16,6 +20,16 @@ class Product(models.Model):
     description = models.TextField(blank=False)
 
 
+class FavoriteList(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+
+
+class ProductImage(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    image = models.ImageField()
+
+
 class Category(models.Model):
     category_title = models.CharField(max_length=15, blank=False)
 
@@ -23,6 +37,16 @@ class Category(models.Model):
 class ProductCategory(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     category_id = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Seller(models.Model):
+    deposit_amount = models.FloatField()
+    rank = models.IntegerField()
+
+
+class ProductSeller(models.Model):
+    product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
+    seller_id = models.ForeignKey(Seller, on_delete=models.CASCADE)
 
 
 class ProductDetails(models.Model):
@@ -42,16 +66,37 @@ class Discount(models.Model):
 class Comment(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    seller_id = models.ForeignKey(Seller, on_delete=models.SET_NULL, null=True)
     subject = models.CharField(max_length=15, blank=False)
     content = models.CharField(max_length=100, blank=False)
     created_date = models.DateTimeField(auto_now_add=True)
+    like_count = models.IntegerField(default=0)
+    dislike_count = models.IntegerField(default=0)
+
+
+class CommentAdvantages(models.Model):
+    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    advantage = models.CharField(max_length=50, blank=False)
+
+
+class CommentDisadvantages(models.Model):
+    comment_id = models.ForeignKey(Comment, on_delete=models.CASCADE)
+    disadvantage = models.CharField(max_length=50, blank=False)
 
 
 class Orders(models.Model):
+    order_status = (
+        (1, "processing"),
+        (2, "packing"),
+        (3, "delivered"),
+        (4, "received")
+    )
     customer_id = models.ForeignKey(Customer, on_delete=models.PROTECT)
+    seller_id = models.ForeignKey(Seller, on_delete=models.PROTECT)
     order_date = models.DateTimeField(auto_now_add=True)
     total_price = models.FloatField()
     discount_amount = models.FloatField()
+    status = models.IntegerField(choices=order_status)
 
 
 class Shipments(models.Model):
@@ -64,10 +109,33 @@ class OrderList(models.Model):
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
     price = models.FloatField()
     number = models.IntegerField()
+    discount = models.FloatField()
+
+
+class Payments(models.Model):
+    payment_status = (
+        (1, "paid"),
+        (2, "unpaid")
+    )
+    order_id = models.ForeignKey(Orders, on_delete=models.CASCADE)
+    amount = models.FloatField()
+    status = models.IntegerField(choices=payment_status)
+    payment_date = models.DateTimeField(auto_now_add=True)
 
 
 class QA(models.Model):
-    subject = models.CharField(max_length=15, blank=False)
+    questinon = models.CharField(max_length=15, blank=False)
     product_id = models.ForeignKey(Product, on_delete=models.CASCADE)
-    content = models.CharField(max_length=100, blank=False)
+    answer = models.CharField(max_length=100, null=True)
     customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+
+
+class Ticket(models.Model):
+    customer_id = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+
+
+class TicketThread(models.Model):
+    ticked_id = models.ForeignKey(QA, on_delete=models.CASCADE)
+    answer_content = models.TextField()
+    question_content = models.TextField()
+    created_ad = models.DateTimeField(auto_now_add=True)
